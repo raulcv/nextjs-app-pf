cb2e730a26484071de2a51a653070b37594d333c
Raul <iraulcv@gmail.com>
2022-03-08 23:26:35 -0500

    uploading v3

--- components/Sections/contactSection.js
+++ components/Sections/contactSection.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import styles from "../../styles/Contact.module.css";
+import styles from "../../styles/Contact.module.scss";
 import * as Unicons from "@iconscout/react-unicons";
 import TextareaAutosize from "react-autosize-textarea";
 import { sendContactMail } from "../../components/Networking/mailApi"  
